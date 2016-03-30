722790192ca522d741a61acb0cab13b5373e9e51
coderjiang <coderjiang@sina.com>
2016-03-30 13:58:24 +0800

    build project

---
+++ src/main/java/demo/Startup.java
@@ -0,0 +1,23 @@
+package demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.jms.JMSException;
+
+/**
+ * Created by jiangjunguo on 3/29/16.
+ */
+public class Startup {
+
+    public static void main(String[] args) throws JMSException {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:activemq-config.xml");
+
+        ProxyJMSConsumer consumer = (ProxyJMSConsumer) ctx.getBean("proxyJMSConsumer");
+
+        consumer.receive();
+
+    }
+
+}
